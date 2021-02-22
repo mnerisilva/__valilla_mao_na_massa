b328b8840baba71b716469959fd7d89cd1dbb946
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-02-22 10:43:30 -0300

    bvbv

--- todo_javascritp_puro/script.js
+++ todo_javascritp_puro/script.js
@@ -7,16 +7,29 @@ buttonElement.onclick = ev => {
     ev.preventDefault();
     if (inputElement.value) {
         const spanTextTarefa = document.createElement('span');
+        spanTextTarefa.classList.add('nomeTarefa');
         spanTextTarefa.innerHTML = inputElement.value;
         const btnRemover = document.createElement('button');
-        btnRemover.innerHTML = 'REMOVER';
+        const btnEditar = document.createElement('button');
+        btnRemover.classList.add('btnRemover');
+        btnEditar.classList.add('btnEditar');
+        btnRemover.innerHTML = '<i class="fas fa-trash-alt"></i>';
+        btnEditar.innerHTML = '<i class="fas fa-pencil-alt"></i>';
         const liElement = document.createElement('li');
         liElement.appendChild(spanTextTarefa);
         liElement.appendChild(btnRemover);
+        liElement.appendChild(btnEditar);
         ulElement.appendChild(liElement);
 
         btnRemover.onclick = () => {
             ulElement.removeChild(liElement);
+        }
+        
+        btnEditar.onclick = () => {
+            //liElement.setAttribute('contentEditable', 'true');
+            const spanTarefa = liElement.querySelector('.nomeTarefa');
+            spanTarefa.setAttribute('contentEditable', 'true');
+            spanTarefa.style.background = 'white';
         }
 
         inputElement.value = '';

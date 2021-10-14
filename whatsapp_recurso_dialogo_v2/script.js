bf485a84f939e9bb108e3a2303e1435ba72b40a8
Marcelo Neri da Silva <marceloneri@gmail.com>
2021-10-14 06:42:30 -0300

    update

--- whatsapp_recurso_dialogo_v2/script.js
+++ whatsapp_recurso_dialogo_v2/script.js
@@ -8,15 +8,21 @@ const nova_div = document.createElement('div');
 const tempos = [3000, 6000, 12000, 18000, 26000, 32000, 38000];
 
 tempos.map((intervalo, index) => {
-    return(
+    //return(
+            let count = 1;
         setTimeout(function(){
             console.log(dialogos[index].pergunta.text)
             //nova_div.innerHTML = `<p>${dialogos[index].pergunta.text}</p>`;
-            dialogo.createElement('p').appen
-            dialogo.querySelector('p:last-child').textContent(`${dialogos[index].pergunta.text}`);
+            let p = document.createElement('p');
+            dialogo.append(p);
+            let ultimo_p = dialogo.querySelector('p');
+            p.classList.add('paragrafo_'+count)
+            count++;
+            console.log(ultimo_p);
+            //ultimo_p.innerHTML(`<span>${dialogos[index].pergunta.text}</span>`);
             //dialogo.append(nova_div);
         },
         intervalo)
-    );
+    //);
 })
 

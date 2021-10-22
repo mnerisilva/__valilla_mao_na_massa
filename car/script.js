eef7f9c65203183c2744b104abf88696227d3d47
mnerisilva <marceloneri@gmail.com>
2021-10-22 16:43:43 -0300

    update

--- car/script.js
+++ car/script.js
@@ -2,10 +2,16 @@ const btnMove = document.querySelector('#move');
 const divImgCar = document.querySelector('.car');
 
 let position = 0;
-btnMove.addEventListener('click', () => {
+//btnMove.addEventListener('click', () => {
+    document.addEventListener('keydown', (e) => {
     position = position - 600;
     document.querySelector('.car').style.backgroundPositionX =  position + 'px' ;
     console.log('cliquei');
     console.log(position);
 
 });
+
+
+document.addEventListener('keydown', (e) => {
+    console.log(e.key)
+})

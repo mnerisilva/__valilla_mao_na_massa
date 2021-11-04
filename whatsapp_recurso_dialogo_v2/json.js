d80394ac2718708feff4a9645514fce685e75b78
mnerisilva <marceloneri@gmail.com>
2021-11-04 14:00:53 -0300

    update

--- whatsapp_recurso_dialogo_v2/json.js
+++ whatsapp_recurso_dialogo_v2/json.js
@@ -1,43 +1,61 @@
 export const dialogos = [
     {
         personagem: '1',
-        pergunta: {text: "Olá"}
+        pergunta: {text: "Olá!"},
+        questao_vinculada: 0,
+        id_questao_vinculada: ""
 
     },
     {
         personagem: '1',
-        pergunta: {text: "Você pode salvar aquele arquivo da apresentação de modo que eu possa acessá-lo de todos os lugares?"}
+        pergunta: {text: "Você pode salvar aquele arquivo da apresentação de modo que eu possa acessá-lo de todos os lugares?"},
+        questao_vinculada: 1,
+        questao_vinculada: [
+            questao1: 
+        ]
     },
     {   
         personagem: '2',
-        pergunta: {text: "Claro! Só não sei como fazer isto? Alguma sugestão?"}
+        pergunta: {text: "Claro! Só não sei como fazer isto? Alguma sugestão?"},
+        questao_vinculada: 0,
+        id_questao_vinculada: ""
     },
     {
 
         personagem: '2',
-        pergunta: {text: "Eu tava pensando num pendrive..."}
+        pergunta: {text: "Eu tava pensando num pendrive..."},
+        questao_vinculada: 0,
+        id_questao_vinculada: ""
 
     },
     {
         personagem: '1',
-        pergunta: {text: "Acho que não! Existem formas mais modernas de disponibilizar conteúdo."}
+        pergunta: {text: "Acho que não! Existem formas mais modernas de disponibilizar conteúdo."},
+        questao_vinculada: 0,
+        id_questao_vinculada: ""
 
     },
     {
         personagem: '2',
-        pergunta: {text: "Então, me sugira algo, por favor!"}
+        pergunta: {text: "Então, me sugira algo, por favor!"},
+        questao_vinculada: 0,
+        id_questao_vinculada: ""
 
     },
     {
 
         personagem: '1',
-        pergunta: {text: "Já ouviu falar em nuvem?"}
+        pergunta: {text: "Já ouviu falar em nuvem?"},
+        questao_vinculada: 0,
+        id_questao_vinculada: ""
 
     },
     {
 
         personagem: '2',
-        pergunta: {text: "Já ouvi falar, sim. Mas não sei bem direito o que é."}
+        pergunta: {text: "Já ouvi falar, sim. Mas não sei bem direito o que é."},
+        questao_vinculada: 0,
+        id_questao_vinculada: ""
 
     }
 
